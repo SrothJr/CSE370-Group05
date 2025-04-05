2bb9611aa9be968f09888f31b931d4790e40466f
SrothJr <nazimhossain14602@gmail.com>
2025-04-05 21:04:54 +0600

    Providing and Collecting resources

---
+++ collect.php
@@ -0,0 +1,48 @@
+<?php
+    include("header.html");
+    include("database.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="post">
+        <h2>Collect your sauces</h2>
+        Course Code: <br>
+        <input type="text" name="course_code"> <br>
+        <input type="submit" name="submit" value="submit"><br>
+</body>
+</html>
+
+
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+    $course_code = filter_input(INPUT_POST, "course_code", FILTER_SANITIZE_SPECIAL_CHARS);
+
+    $sql = "SELECT * FROM resources WHERE course_code='$course_code'";
+
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_assoc($result);
+        echo '<a href="' . htmlspecialchars($row['books']) . '">Books</a>' . '<br>';
+        echo '<a href="' . htmlspecialchars($row['lectures']) . '">Lectures</a>' . "<br>";
+        echo '<a href="' . htmlspecialchars($row['videos']) . '">Videos</a>'. "<br>";
+
+    }
+    else {
+        echo "NO RESOURCES FOUND";
+    }
+}
+
+
+
+    mysqli_close($conn)
+?>
---
+++ provide.php
@@ -0,0 +1,60 @@
+<?php
+    include("header.html");
+    include("database.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="post">
+        <h2>Provide your sauces</h2>
+        Course Code: <br>
+        <input type="text" name="course_code"> <br>
+        Course Title: <br>
+        <input type="text" name="course_title"> <br>
+        Link of Books folder: <br>
+        <input type="text" name="books"> <br>
+        Link of Lectures folder: <br>
+        <input type="text" name="lectures"> <br>
+        Link of Video folder: <br>
+        <input type="text" name="videos"><br>
+        <input type="submit" name="submit" value="submit"> <br>
+
+
+    </form>
+</body>
+</html>
+
+
+
+
+<?php
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $course_code = filter_input(INPUT_POST, "course_code", FILTER_SANITIZE_SPECIAL_CHARS);
+        $course_title = filter_input(INPUT_POST, "course_title", FILTER_SANITIZE_SPECIAL_CHARS);
+        $books = filter_input(INPUT_POST, "books", FILTER_SANITIZE_SPECIAL_CHARS);
+        $lectures = filter_input(INPUT_POST, "lectures", FILTER_SANITIZE_SPECIAL_CHARS);
+        $videos = filter_input(INPUT_POST, "videos", FILTER_SANITIZE_SPECIAL_CHARS);
+
+        $sql = "INSERT INTO resources (course_code, course_title, books, lectures, videos) VALUES ('$course_code', '$course_title','$books', '$lectures', '$videos')";
+
+        try{
+            mysqli_query($conn, $sql);
+            echo "Successfully Provided";
+            }
+            catch(mysqli_sql_exception) {
+                echo "Could not provide";
+            }
+
+
+    }
+
+
+    mysqli_close($conn);
+?>
