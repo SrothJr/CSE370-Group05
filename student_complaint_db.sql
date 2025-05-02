a984d5785db3786d3899545412e869fd49a333ab
Zoye-J <zoye.amber@gmail.com>
2025-05-03 01:59:14 +0600

    Add files via upload
    latest latest database (with course_review added)
    The course review file with sorting feature

--- student_complaint_db.sql
+++ student_complaint_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 02, 2025 at 07:29 PM
+-- Generation Time: May 02, 2025 at 09:54 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -60,7 +60,31 @@ INSERT INTO `complaints` (`id`, `title`, `description`, `category`, `status`, `c
 (5, 'Slow Wifi', 'Library Wifi is slow', 'student_services', 'Pending', '2025-05-02 14:19:52', NULL),
 (6, 'More Buses needed', 'Need more Buses for Mohammadpur', 'facilities', 'Pending', '2025-05-02 14:55:42', NULL),
 (8, 'Bring back BuX', 'It was helpful', 'academic', 'Pending', '2025-05-02 14:56:28', NULL),
-(9, 'Cafe Food Quality', 'It needs improvement', 'others', 'Pending', '2025-05-02 14:56:55', NULL);
+(9, 'Cafe Food Quality', 'It needs improvement', 'others', 'Pending', '2025-05-02 14:56:55', NULL),
+(12, 'We need more seats', 'More seats for important courses are needed.', 'academic', 'Pending', '2025-05-02 19:39:33', NULL),
+(13, 'More seats needed', 'We need more seats in important courses.', 'academic', 'Pending', '2025-05-02 19:42:10', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `courses`
+--
+
+CREATE TABLE `courses` (
+  `id` int(11) NOT NULL,
+  `course_code` varchar(20) NOT NULL,
+  `course_title` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `courses`
+--
+
+INSERT INTO `courses` (`id`, `course_code`, `course_title`) VALUES
+(1, 'CSE110', 'Introduction to Programming'),
+(2, 'CSE111', 'Structured Programming Language'),
+(3, 'CSE220', 'Data Structures'),
+(4, 'CSE230', 'Algorithms');
 
 -- --------------------------------------------------------
 
@@ -72,10 +96,20 @@ CREATE TABLE `course_reviews` (
   `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `course_code` varchar(20) NOT NULL,
+  `course_title` varchar(255) DEFAULT NULL,
   `rating` int(11) NOT NULL CHECK (`rating` >= 1 and `rating` <= 5),
   `comment` text DEFAULT NULL,
   `review_date` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `course_reviews`
+--
+
+INSERT INTO `course_reviews` (`id`, `user_id`, `course_code`, `course_title`, `rating`, `comment`, `review_date`) VALUES
+(1, 1, 'CSE110', NULL, 4, 'Nice', '2025-05-02 17:51:33'),
+(2, 1, 'CSE220', NULL, 3, 'Too hard!!!', '2025-05-02 19:17:24'),
+(3, 1, 'CSE111', NULL, 3, 'It\'s okay', '2025-05-02 19:43:08');
 
 -- --------------------------------------------------------
 
@@ -134,7 +168,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `created_at`, `phone`) VALUES
-(1, 'Atom Eve', 'zoye.amber@gmail.com', '$2y$10$gUW1qDY51LRsEewVrDUhTeYJw8JJptLwXOB/6IwHIxnm0y/GixNsy', '2025-03-26 18:55:57', '01757467619'),
+(1, 'Atom Eve', 'zoye.amber@gmail.com', '$2y$10$U69BH8nAOCvl1Jyp5m.4Oep0YYoIeUyEPS4mKT6WqNGLvzli1.TVW', '2025-03-26 18:55:57', '01757467619'),
 (2, 'Zoye Jahin', 'zoye.jahin@gmail.com', '$2y$10$5uA.xooG1FFeUL975Z3IPeGuJJcgkh9pOBAH/4Rs/.CWzI5Wf6PZO', '2025-03-26 20:13:59', '');
 
 -- --------------------------------------------------------
@@ -162,7 +196,9 @@ INSERT INTO `votes` (`id`, `complaint_id`, `user_id`, `vote_type`) VALUES
 (5, 5, 1, 'upvote'),
 (6, 9, 1, 'upvote'),
 (7, 8, 1, 'downvote'),
-(8, 6, 1, 'upvote');
+(8, 6, 1, 'upvote'),
+(9, 11, 1, 'upvote'),
+(10, 13, 1, 'upvote');
 
 --
 -- Indexes for dumped tables
@@ -179,6 +215,13 @@ ALTER TABLE `adminsee`
 --
 ALTER TABLE `complaints`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `courses`
+--
+ALTER TABLE `courses`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `course_code` (`course_code`);
 
 --
 -- Indexes for table `course_reviews`
@@ -227,13 +270,19 @@ ALTER TABLE `adminsee`
 -- AUTO_INCREMENT for table `complaints`
 --
 ALTER TABLE `complaints`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `courses`
+--
+ALTER TABLE `courses`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `course_reviews`
 --
 ALTER TABLE `course_reviews`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `faqs`
@@ -251,7 +300,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `votes`
 --
 ALTER TABLE `votes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- Constraints for dumped tables
